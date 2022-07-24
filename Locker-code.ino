23aefb29f1ec9c0c2491e133bbe85d93e98292a8
Indranil Sen <95271465+Indranil0603@users.noreply.github.com>
2022-07-24 21:18:31 +0530

    Update Locker-code.ino

--- Locker-code.ino
+++ Locker-code.ino
@@ -1,73 +1,88 @@
-#include <Servo.h>
 #include <EEPROM.h>
-#define MORSE_PIN 2
-#define Enter_PIN 4
-#define Reset_PIN 7
-unsigned long start_t=0, end_t=0 ;
-int dot = 200;
+#include <Servo.h>
+#define DOT_PIN 2
+#define DASH_PIN 3
+#define Word_PIN 4
+#define ENTER_PIN 5
+#define RESET_PIN 7
+#define RESET_LED 9
 String letterCode = "";
 String Word = "";
-String Password = EEPROM.read(0);
+String Password = "AB";
 bool Open = false;
 bool reset = false;
 Servo MyServo;
 
 void setup() {
-  pinMode(MORSE_PIN , INPUT);
-  pinMode(Enter_PIN, INPUT);
-  MyServo.attach(6)
-  pinMode(Reset_PIN, INPUT);
-
+  pinMode(DOT_PIN, INPUT_PULLUP); // setup button used to enter dot
+  pinMode(DASH_PIN, INPUT_PULLUP); // setup button used to enter dash
+  pinMode(Word_PIN, INPUT_PULLUP); // setup button used to get the word for the morse code
+  pinMode(ENTER_PIN, INPUT_PULLUP);// setup button used to open the lock with correct password and close the lock
+  pinMode(RESET_PIN, INPUT_PULLUP);// setup button used to activate the reset password mode for the locker
+  pinMode(RESET_LED, OUTPUT);// setup for led which glows when reset mode is activated
+  MyServo.attach(6);
 }
 
 void loop() {
-  //for entering the morse code
-  if(signal1 && !reset){
-    morsecodeinput();
+  
+  if(digitalRead(DOT_PIN) == LOW){ // This statement executes when dot button is pressed
+    letterCode += '.';
+    delay(300);
+  }else if(digitalRead(DASH_PIN) == LOW){ // Thus statement executes when the dash button is pressed
+    letterCode += '-';
+    delay(300);
   }
-  //Multifunctional Enter switch by which you can enter password and even clear
-  if(digitalRead(Enter_PIN) == HIGH){
-    EnterSwitch(); 
+  else if(digitalRead(Word_PIN) == LOW){ // This statement executes when the letter button is pressed
+    Word += Alphabet(letterCode);
+    letterCode = "";
+    delay(300);
   }
-  //Opening the lock
-  LockOpen();
-  // Reset switch for cahnging password
-  if(digitalRead(Reset_PIN) == HIGH){
-    if(reset){
-      EEPROM.update(0,Word);
+  else if(digitalRead(ENTER_PIN) == LOW){ // This statement executes when the enter button is pressed
+    EnterSwitch();
+    delay(300);
+  }
+  else if((digitalRead(RESET_PIN)== LOW )&& Open){ // This statement executes when the reset button is pressed
+    ResetSwitch();
+    delay(300);
+  }
+  LockOpen(); // This statement opens the lock when open is true
+  if(reset == true){ //This turns on the LED when reset is true
+    digitalWrite(RESET_LED, HIGH); 
+  } 
+
+}
+//This function gives charecter for morse code
+char Alphabet(String code){
+  char Alphabets[26] = {'A','B','C','D','E','F','G','H','I','J','K',
+                        'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+  String Alphabetcode[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
+                              "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+  int flag = 0;
+  for(int i =0 ; i<26; i++){
+    if (code == Alphabetcode[i]){
+      return Alphabets[i];
+      flag +=1; 
     }
-    reset = !reset;
-  }
-  //When reset switch is pressed it works
-  if(reset){
-    if(Open){
-      ResetSwitch();
-    }
-  }
+  }if(flag == 0){ 
+      return ;
+      }
 }
 
-//takes the morse code of new password
-void ResetSwitch(){
-  if(!reset){
-    reset = true;
-    }
-  if(signal1){
-    morsecodeinput();
-    }
-  }
-  
-//Matches the password with the code entered
+//This is function for enter switch that is used to open/close the lock
 void EnterSwitch(){
-  Password = EEPROM.read(0);
+  Password = readStringFromEEPROM(1);
   if(Word == Password){
     Open = true;
+    letterCode = "";
     Word = "";
   }else{
-    Word = ""
+    Word = "";
     Open = false;
+    letterCode= "";
     }
 }
-//Opens/closes the lock by triggering the servo motor
+
+//Opens/closes the lock by trigering the servo motor
 void LockOpen(){
   if(Open){
     MyServo.write(90);
@@ -75,49 +90,42 @@ void LockOpen(){
     MyServo.write(0);
   }
 }
-//Takes the morse code input
-void morsecodeinput(){
-  start_t = millis();
-  while(signal1()){}
-  end_t = millis();
 
-  unsigned long duration = end_t - start_t;//This gives us the time for which the switch was pressed
-  char MorseChar = DotDash(duration);
-  letterCode += MorseChar; //This stores the . or - in the letter code
-  
-  while(millis()- end_t < dot*3){//if the user does not press the morse code switch alphabet will be rendered
-    if( signal1()){
-      return;
-      }
-  }
-  Word+= Alphabet(letterCode);
-  letterCode = "";
-}
-//This function gives charector for morse code
-char Alphabet(String code){
-  char Alphabets[26] = {'A','B','C','D','E','F','G','H','I','J','K',
-                        'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-  String Alphabetcode[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
-                              "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-  for(int i =0 ; i<26; i++){
-    if (code == Alphabetcode[i]){
-      return Alphabets[i]; 
-      }
-    }else { return '';}
-}
-//Returns the signal if the Morse oin is pressed
-bool signal1(){
-  if( digitalRead(MORSE_PIN) == HIGH){
-    return true;
-  }else{
-    return false;
+//function to write the password in EEPROM
+void writeStringToEEPROM(int addrOffset, const String &strToWrite)
+{
+  byte len = strToWrite.length();
+  EEPROM.write(addrOffset, len);
+  for (int i = 0; i < len; i++)
+  {
+    EEPROM.write(addrOffset + 1 + i, strToWrite[i]);
   }
 }
-//This returns dot or dash on based on the duration of which switch was pressed
-char DotDash(unsigned long duration){// this returns dot or dash on based on the duration of which switch was pressed
-  if(duration < dot*1.5){
-    return '.';
-  }else if(duration > dot*2.5){
-    return '-';
+
+//function to read the password from the EEPROM
+String readStringFromEEPROM(int addrOffset)
+{
+  int newStrLen = EEPROM.read(addrOffset);
+  char data[newStrLen + 1];
+  for (int i = 0; i < newStrLen; i++)
+  {
+    data[i] = EEPROM.read(addrOffset + 1 + i);
   }
+  data[newStrLen] = '\0'; 
+  return String(data);
 }
+
+//function that works when reset button is pressed
+void ResetSwitch(){
+  if(!reset){
+    reset = true;
+    }
+  else if(reset){
+    writeStringToEEPROM(1, Word);
+    Word = "";
+    letterCode = "";
+    reset = false;
+    }
+    digitalWrite(RESET_LED, LOW);
+  delay(400);
+  }
